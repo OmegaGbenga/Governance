073d19f14b9e5facd1c922ac1d8ef9819596f35e
omegagbenga <Gbengadareowolabi@gmail.com>
2025-12-30 00:57:14 +0100

    feat(marketplace): add Component.jsx [decqpl]

--- src/features/Marketplace/Component.jsx
+++ src/features/Marketplace/Component.jsx
@@ -1,3 +1,1 @@
-import React from 'react';
-import styles from './styles.module.css';
-export const MarketplaceComponent = () => <div className={styles.container}>Marketplace</div>;
+import React from 'react'; export const MarketplaceComponent = () => <div>Marketplace</div>;
