56b1ef7293f4f9524a3edb3647380611bfc1ce82
omegagbenga <Gbengadareowolabi@gmail.com>
2025-12-30 00:56:50 +0100

    feat(voting): add Component.jsx [dq4c5t]

--- src/features/Voting/Component.jsx
+++ src/features/Voting/Component.jsx
@@ -1,3 +1,1 @@
-import React from 'react';
-import styles from './styles.module.css';
-export const VotingComponent = () => <div className={styles.container}>Voting</div>;
+import React from 'react'; export const VotingComponent = () => <div>Voting</div>;
