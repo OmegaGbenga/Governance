34144fffdb61e96b2e655a5469bbe82ebfe2967d
omegagbenga <Gbengadareowolabi@gmail.com>
2025-12-30 00:55:34 +0100

    feat(swap): add Component.jsx [bxirnj]

--- src/features/Swap/Component.jsx
+++ src/features/Swap/Component.jsx
@@ -1,3 +1,1 @@
-import React from 'react';
-import styles from './styles.module.css';
-export const SwapComponent = () => <div className={styles.container}>Swap</div>;
+import React from 'react'; export const SwapComponent = () => <div>Swap</div>;
