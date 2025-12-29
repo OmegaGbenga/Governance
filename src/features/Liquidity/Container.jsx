ee41376c8d711fbbae52d3dcb9ddd8eef18cacf0
omegagbenga <Gbengadareowolabi@gmail.com>
2025-12-30 00:56:00 +0100

    feat(liquidity): add Container.jsx [igyohv]

--- src/features/Liquidity/Container.jsx
+++ src/features/Liquidity/Container.jsx
@@ -1,3 +1,1 @@
-import React from 'react';
-import { LiquidityComponent } from './Component';
-export const LiquidityContainer = () => <LiquidityComponent />;
+import React from 'react'; import { LiquidityComponent } from './Component'; export const LiquidityContainer = () => <LiquidityComponent />;
