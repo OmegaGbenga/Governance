be966d245936f0a2c65add124b81477215735a62
omegagbenga <Gbengadareowolabi@gmail.com>
2025-12-30 00:56:23 +0100

    feat(farming): add Component.jsx [a2c01q]

--- src/features/Farming/Component.jsx
+++ src/features/Farming/Component.jsx
@@ -1,3 +1,1 @@
-import React from 'react';
-import styles from './styles.module.css';
-export const FarmingComponent = () => <div className={styles.container}>Farming</div>;
+import React from 'react'; export const FarmingComponent = () => <div>Farming</div>;
