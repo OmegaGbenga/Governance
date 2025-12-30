1575a5b8b95f43ce1065ec42cbf2f28b81e51fcb
omegagbenga <Gbengadareowolabi@gmail.com>
2025-12-30 01:06:38 +0100

    feat(onboarding): add Component.jsx [1mw7fh]

--- src/features/Onboarding/Component.jsx
+++ src/features/Onboarding/Component.jsx
@@ -1,3 +1,1 @@
-import React from 'react';
-import styles from './styles.module.css';
-export const OnboardingComponent = () => <div className={styles.container}>Onboarding</div>;
+import React from 'react'; export const OnboardingComponent = () => <div>Onboarding</div>;
