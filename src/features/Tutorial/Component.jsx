2ae5704ee413d0b5c6699ce797667dcaad98cce6
omegagbenga <Gbengadareowolabi@gmail.com>
2025-12-30 01:06:11 +0100

    feat(tutorial): add Component.jsx [49migd]

--- src/features/Tutorial/Component.jsx
+++ src/features/Tutorial/Component.jsx
@@ -1,3 +1,1 @@
-import React from 'react';
-import styles from './styles.module.css';
-export const TutorialComponent = () => <div className={styles.container}>Tutorial</div>;
+import React from 'react'; export const TutorialComponent = () => <div>Tutorial</div>;
