80e0b0fe34f9a82ba32836eba862b5a4fd81071b
omegagbenga <Gbengadareowolabi@gmail.com>
2025-12-30 01:00:56 +0100

    feat(backup): add Component.jsx [qzwsth]

--- src/features/Backup/Component.jsx
+++ src/features/Backup/Component.jsx
@@ -1,3 +1,1 @@
-import React from 'react';
-import styles from './styles.module.css';
-export const BackupComponent = () => <div className={styles.container}>Backup</div>;
+import React from 'react'; export const BackupComponent = () => <div>Backup</div>;
