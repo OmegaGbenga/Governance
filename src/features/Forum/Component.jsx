f9e8c85235c8df86dd17c73fd14e2a57366dc1da
omegagbenga <Gbengadareowolabi@gmail.com>
2025-12-30 01:04:16 +0100

    feat(forum): add Component.jsx [8mjmfr]

--- src/features/Forum/Component.jsx
+++ src/features/Forum/Component.jsx
@@ -1,3 +1,1 @@
-import React from 'react';
-import styles from './styles.module.css';
-export const ForumComponent = () => <div className={styles.container}>Forum</div>;
+import React from 'react'; export const ForumComponent = () => <div>Forum</div>;
