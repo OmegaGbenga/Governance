824bc41e49a2a4c5eea333bd82566c92d4d774c2
omegagbenga <Gbengadareowolabi@gmail.com>
2025-12-30 01:08:02 +0100

    feat(jobs): add Component.jsx [pas58]

--- src/features/Jobs/Component.jsx
+++ src/features/Jobs/Component.jsx
@@ -1,3 +1,1 @@
-import React from 'react';
-import styles from './styles.module.css';
-export const JobsComponent = () => <div className={styles.container}>Jobs</div>;
+import React from 'react'; export const JobsComponent = () => <div>Jobs</div>;
