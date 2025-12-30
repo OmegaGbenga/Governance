01d557a3e47228da470ea6cf6ee6fcdd64d694eb
omegagbenga <Gbengadareowolabi@gmail.com>
2025-12-30 01:07:33 +0100

    feat(roadmap): add Component.jsx [jv7s3]

--- src/features/Roadmap/Component.jsx
+++ src/features/Roadmap/Component.jsx
@@ -1,3 +1,1 @@
-import React from 'react';
-import styles from './styles.module.css';
-export const RoadmapComponent = () => <div className={styles.container}>Roadmap</div>;
+import React from 'react'; export const RoadmapComponent = () => <div>Roadmap</div>;
