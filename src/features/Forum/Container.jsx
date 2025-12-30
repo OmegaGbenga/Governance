298ed71927eb418d660fe4b7a964272035514307
omegagbenga <Gbengadareowolabi@gmail.com>
2025-12-30 01:04:16 +0100

    feat(forum): add Container.jsx [jbrde]

--- src/features/Forum/Container.jsx
+++ src/features/Forum/Container.jsx
@@ -1,3 +1,1 @@
-import React from 'react';
-import { ForumComponent } from './Component';
-export const ForumContainer = () => <ForumComponent />;
+import React from 'react'; import { ForumComponent } from './Component'; export const ForumContainer = () => <ForumComponent />;
