5edc3f550cc061982e8cd37b037f418a561db752
omegagbenga <Gbengadareowolabi@gmail.com>
2025-12-30 01:05:14 +0100

    feat(help): add Container.jsx [gjtia]

--- src/features/Help/Container.jsx
+++ src/features/Help/Container.jsx
@@ -1,3 +1,1 @@
-import React from 'react';
-import { HelpComponent } from './Component';
-export const HelpContainer = () => <HelpComponent />;
+import React from 'react'; import { HelpComponent } from './Component'; export const HelpContainer = () => <HelpComponent />;
